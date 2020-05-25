dadea30a115c413c0bc88823aebce211b3ff6b10
sergey vanteev <fukkrypto@gmail.com>
2020-05-25 17:06:30 +0300

    некоторая часть логики перенесена в signals.py

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -21,8 +21,8 @@ def allowed_users(allowed_roles=[]):
 			if group in allowed_roles:
 				return view_func(request, *args, **kwargs)
 			else:
-				return HttpResponse('You are not authorized to view this page')
-				 #return redirect('user-page')
+				#return HttpResponse('You are not authorized to view this page')
+				 return redirect('user-page')
 		return wrapper_func
 	return decorator
 
